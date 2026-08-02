1cc15e4cad647baf8d28b2c95a370fec4b83c487
huyl121 <381471618@qq.com>
2026-08-02 15:15:06 +0800

    bat升级，分3步操作，停止-copy-启动

--- src/main/java/com/ChongQi/QiDongJava.java
+++ src/main/java/com/ChongQi/QiDongJava.java
@@ -37,11 +37,22 @@ public class QiDongJava {
             Callable callable1 = new Callable() {
                 @Override
                 public String call() throws Exception {
-                    JSONObject config1 = PrivateConfig.readJsonFile(path);
-                    String computer = config1.getString("computer");
-                    String ip = config1.getString("ip");
-                    try{
-                        //替换
+                    return mo(directory, path, token);
+                }
+            };
+            threadPoolExecutor.submit(callable1);
+            Thread.sleep(50);
+
+        };
+    }
+
+
+    public String mo(String directory, String path, JSONObject token){
+        JSONObject config1 = PrivateConfig.readJsonFile(path);
+        String computer = config1.getString("computer");
+        String ip = config1.getString("ip");
+        try{
+            //替换
                     /*try (FileWriter writer = new FileWriter(path)) {
                         writer.write(config.toJSONString());
                         System.out.println( computer + "：token替换成功");
@@ -50,105 +61,103 @@ public class QiDongJava {
                         System.out.println(computer + "：token替换失败");
                     }*/
 
-                        String user = "root";
-                        String host = ip;
-                        int port = 22; // SSH通常使用22端口
-                        String privateKey = "/path/to/your/private/key"; // 如果使用密码认证，则不需要此参数
-                        String password = "4mWkCV88cKBJzPy"; // 如果使用密钥认证，则不需要此参数
+            String user = "root";
+            String host = ip;
+            int port = 22; // SSH通常使用22端口
+            String privateKey = "/path/to/your/private/key"; // 如果使用密码认证，则不需要此参数
+            String password = "4mWkCV88cKBJzPy"; // 如果使用密钥认证，则不需要此参数
 //                    String command = "nohup java -jar /root/huyl/bushu/bian-0.0.1-tongYong.jar /root/huyl/bushu bianGenDan " + token.getString("token") + " --server.port=10184 &";
 //                    String command = " source /etc/profile; nohup /root/huyl/jdk1.8.0_181/bin/java -jar /root/huyl/bushu/bian-0.0.1-tongYong.jar /root/huyl/bushu bianGenDan "
 //                            + token.getString("token") + "  --server.port=10184 > /root/huyl/bushu/nohup.out 2>&1 &";
 
-                        String command = null;
-                        boolean web = false;
-                        if(StringUtils.isNotBlank(token.getString("cookie"))){
-                            web = true;
-                            command = " source /etc/profile; nohup /root/huyl/jdk1.8.0_141/bin/java -jar /root/huyl/bushu/bian-0.0.1-tongYong.jar /root/huyl/bushu bianGenDan "
-                                    + " web " + " --server.port=10184 >> /root/huyl/bushu/nohup.out 2>&1 &";
-                        }else {
-                            command = " source /etc/profile; nohup /root/huyl/jdk1.8.0_141/bin/java -jar /root/huyl/bushu/bian-0.0.1-tongYong.jar /root/huyl/bushu bianGenDan "
-                                    + token.getString("token") + " --server.port=10184 >> /root/huyl/bushu/nohup.out 2>&1 &";
-                        }
+            String command = null;
+            boolean web = false;
+            if(StringUtils.isNotBlank(token.getString("cookie"))){
+                web = true;
+                command = " source /etc/profile; nohup /root/huyl/jdk1.8.0_141/bin/java -jar /root/huyl/bushu/bian-0.0.1-tongYong.jar /root/huyl/bushu bianGenDan "
+                        + " web " + " --server.port=10184 >> /root/huyl/bushu/nohup.out 2>&1 &";
+            }else {
+                command = " source /etc/profile; nohup /root/huyl/jdk1.8.0_141/bin/java -jar /root/huyl/bushu/bian-0.0.1-tongYong.jar /root/huyl/bushu bianGenDan "
+                        + token.getString("token") + " --server.port=10184 >> /root/huyl/bushu/nohup.out 2>&1 &";
+            }
 
 
-                        JSch jsch = new JSch();
-                        Session session = jsch.getSession(user, host, port);
-                        session.setPassword(password);
+            JSch jsch = new JSch();
+            Session session = jsch.getSession(user, host, port);
+            session.setPassword(password);
 
-                        // 跳过主机密钥检查，实际应用中请确保安全性
-                        session.setConfig("StrictHostKeyChecking", "no");
+            // 跳过主机密钥检查，实际应用中请确保安全性
+            session.setConfig("StrictHostKeyChecking", "no");
 
-                        session.connect();
+            session.connect();
 
-                        ChannelExec channel = (ChannelExec) session.openChannel("exec");
+            ChannelExec channel = (ChannelExec) session.openChannel("exec");
 
-                        //上传配置文件
-                        if(web){
-                            uploadFile(session, directory + "/token.json", "/root/huyl/bushu/token.json");
-                            System.out.println("token上传成功");
-                            Thread.sleep(1000);
-                        }
+            //上传配置文件
+            if(web){
+                uploadFile(session, directory + "/token.json", "/root/huyl/bushu/token.json");
+                System.out.println(computer + ":token上传成功");
+                Thread.sleep(1000);
+            }
 
-                        //执行命令
-                        channel.setCommand(command);
+            //执行命令
+            channel.setCommand(command);
 
-                        channel.setInputStream(null);
-                        ((ChannelExec) channel).setErrStream(System.err);
+            channel.setInputStream(null);
+            ((ChannelExec) channel).setErrStream(System.err);
 
-                        InputStream in = channel.getInputStream();
-                        channel.connect();
+            InputStream in = channel.getInputStream();
+            channel.connect();
 
-                        byte[] tmp = new byte[1024];
-                        while (true) {
-                            while (in.available() > 0) {
-                                int i = in.read(tmp, 0, 1024);
-                                if (i < 0) break;
-                                String msg = new String(tmp, 0, i);
-                                System.out.print(msg);
-                            }
-                            if (channel.isClosed()) {
+            byte[] tmp = new byte[1024];
+            while (true) {
+                while (in.available() > 0) {
+                    int i = in.read(tmp, 0, 1024);
+                    if (i < 0) break;
+                    String msg = new String(tmp, 0, i);
+                    System.out.print(msg);
+                }
+                if (channel.isClosed()) {
 //                                System.out.println("exit-status: " + channel.getExitStatus());
-                                break;
-                            }
-                            try {
-                                Thread.sleep(1000);
-                            } catch (Exception ee) {
-                            }
-                        }
-                        channel.disconnect();
-                        session.disconnect();
-                        System.out.println(computer + "：启动成功了");
-                        return "1";
-                    }catch (Exception e){
-                        System.out.println(computer + "：启动失败了");
-                        return "0";
-                    }
+                    break;
                 }
-
-                /**
-                 * SFTP上传文件
-                 */
-                public void uploadFile(Session session, String localFile, String remoteFile) throws JSchException, SftpException {
-                    ChannelSftp sftp = (ChannelSftp) session.openChannel("sftp");
-                    sftp.connect();
-
-                    File local = new File(localFile);
-                    try (InputStream in = new FileInputStream(local)) {
-                        // 覆盖上传
-                        sftp.put(in, remoteFile, ChannelSftp.OVERWRITE);
-                    } catch (FileNotFoundException e) {
-                        throw new RuntimeException(e);
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
-
-                    sftp.disconnect();
+                try {
+                    Thread.sleep(1000);
+                } catch (Exception ee) {
                 }
-            };
-            threadPoolExecutor.submit(callable1);
-            Thread.sleep(50);
-
+            }
+            channel.disconnect();
+            session.disconnect();
+            System.out.println(computer + "：启动成功了");
+            return "1";
+        }catch (Exception e){
+            System.out.println(computer + "：启动失败了");
+            return "0";
         }
     }
+
+
+
+    /**
+     * SFTP上传文件
+     */
+    public void uploadFile(Session session, String localFile, String remoteFile) throws JSchException, SftpException {
+        ChannelSftp sftp = (ChannelSftp) session.openChannel("sftp");
+        sftp.connect();
+
+        File local = new File(localFile);
+        try (InputStream in = new FileInputStream(local)) {
+            // 覆盖上传
+            sftp.put(in, remoteFile, ChannelSftp.OVERWRITE);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        sftp.disconnect();
+    }
+
+
 
 }
--- src/main/java/com/ChongQi/shengJi.java
+++ src/main/java/com/ChongQi/shengJi.java
@@ -1,41 +1,92 @@
 package com.ChongQi;
 
-import com.jcraft.jsch.ChannelExec;
-import com.jcraft.jsch.JSch;
-import com.jcraft.jsch.Session;
-import java.io.InputStream;
+import com.alibaba.fastjson.JSONObject;
+import com.example.bian.client.bushu.PrivateConfig;
+import com.google.gson.JsonObject;
+import com.jcraft.jsch.*;
+import org.apache.commons.lang.StringUtils;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
 
 public class shengJi {
-    public static void main(String[] args) throws Exception {
+
+    public static void main(String[] args) throws InterruptedException {
+        shengJi chongQi = new shengJi();
+        chongQi.method("E://huyl//0bat");
+
+    }
+
+
+    public void method(String directory) throws InterruptedException {
+        List<String> paths = new ArrayList<>();
+        File1.DirectoryScanner(directory, paths);
+        for(String path : paths){
+            System.out.println(path);
+        }
+        ThreadPoolExecutor threadPoolExecutor =
+                new ThreadPoolExecutor(paths.size(), paths.size(), 10,
+                        TimeUnit.SECONDS,
+                        new LinkedBlockingQueue<>(),
+                        Executors.defaultThreadFactory(),
+                        new ThreadPoolExecutor.DiscardPolicy());
+        JSONObject token = PrivateConfig.readJsonFile(directory + "/token.json");
+        for(String path : paths){
+            Callable callable1 = new Callable() {
+                @Override
+                public String call() throws Exception {
+                    JSONObject config1 = PrivateConfig.readJsonFile(path);
+                    String computer = config1.getString("computer");
+                    String ip = config1.getString("ip");
+
+                    try{
+                        zhiXing(ip, computer, directory, path, token);
+                    }catch (Exception e){
+                        e.printStackTrace();
+                    }catch (Throwable throwable){
+                        throwable.printStackTrace();
+                    }
+                    return "1";
+                }
+            };
+            threadPoolExecutor.submit(callable1);
+            Thread.sleep(50);
+
+        }
+
+
+    }
+    public  void zhiXing(String host, String computer, String directory, String path, JSONObject token) throws Exception {
         String user = "root";
-        String host = "你的IP";
         int port = 22;
         String password = "4mWkCV88cKBJzPy";
 
         // ========== 1. 定义多条需要执行的命令 ==========
         String[] cmdList = new String[]{
-                "source /etc/profile; echo 第一条命令执行成功",
-                "source /etc/profile; mkdir -p /root/huyl/test",
-                "source /etc/profile; nohup /root/huyl/jdk1.8.0_141/bin/java -jar /root/huyl/bushu/bian-0.0.1-tongYong.jar /root/huyl/bushu bianGenDan "
-                        + "这里替换你的token" + " --server.port=10184 > /root/huyl/bushu/nohup.out 2>&1 &",
-                "source /etc/profile; echo 程序启动完成，查看日志"
+                "source /etc/profile; pkill -9 -f java ",
+                "source /etc/profile; \\cp -rf /root/huyl/bian-0.0.1-tongYong.jar /root/huyl/bushu/ "
         };
 
         // ========== 2. 循环逐条执行，每条执行完等待1秒 ==========
         for (String command : cmdList) {
-            System.out.println("==================== 开始执行命令：" + command + " ====================");
-            execSingleCmd(user, host, port, password, command);
+//            System.out.println("==================== 开始执行命令：" + command + " ====================");
+            execSingleCmd(user, host, port, password, command, computer);
 
             // 每条命令执行完成后，间隔1秒再执行下一条
-            Thread.sleep(1000);
+            Thread.sleep(2000);
         }
-        System.out.println("所有命令全部执行完毕");
+        System.out.println(computer + ":jar包已升级");
+        Thread.sleep(3000);
+        QiDongJava chongQi = new QiDongJava();
+        chongQi.mo(directory, path, token);
     }
 
     /**
      * 封装：单独执行一条SSH命令，执行完成自动关闭通道和会话
      */
-    public static void execSingleCmd(String user, String host, int port, String password, String command) throws Exception {
+    public static void execSingleCmd(String user, String host, int port, String password, String command, String computer) throws Exception {
         JSch jsch = new JSch();
         Session session = jsch.getSession(user, host, port);
         session.setPassword(password);
@@ -59,7 +110,7 @@ public class shengJi {
                 System.out.print(msg);
             }
             if (channel.isClosed()) {
-                System.out.println("当前命令 exit-status: " + channel.getExitStatus());
+//                System.out.println(computer + " : 当前命令 exit-status: " + channel.getExitStatus());
                 break;
             }
             Thread.sleep(500);
